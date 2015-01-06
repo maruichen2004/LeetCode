e2f72c34bac07bb3457dd1a595b6828e0e589a02
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2015-01-05 21:30:57 -0800

    init commit

--- Find_Minimum_In_Rotated_Sorted_Array_II.py
+++ Find_Minimum_In_Rotated_Sorted_Array_II.py
@@ -2,14 +2,12 @@ class Solution:
     # @param num, a list of integer
     # @return an integer
     def findMin(self, num):
-        res = num[0]
-        l, r = 0, len(num) - 2
-        while l <= r:
+        if len(num) == 0: return 0
+        l, r = 0, len(num) - 1
+        while l < r - 1:
             mid = (l + r) / 2
-            if num[mid] >= res and num[mid+1] <= res and num[mid] > num[mid+1]:
-                return num[mid+1]
-            elif num[mid+1] >= num[mid] and num[mid] >= res:
-                l = mid + 1
-            else:
-                r = mid - 1
-        return res
+            if num[l] < num[r]: return num[l]
+            if num[mid] < num[r]: r = mid
+            elif num[mid] > num[l]: l = mid
+            else: l += 1
+        return min(num[l], num[r])
