bfe46ee11b140bf371dafddb20235d7a39503e42
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2015-01-21 22:01:10 -0800

    init commit

--- Search_A_2D_Matrix.py
+++ Search_A_2D_Matrix.py
@@ -3,9 +3,19 @@ class Solution:
     # @param target, an integer
     # @return a boolean
     def searchMatrix(self, matrix, target):
-        i, j = 0, len(matrix[0]) - 1
-        while i < len(matrix) and j >= 0:
-            if matrix[i][j] == target: return True
-            elif matrix[i][j] > target: j -= 1
-            else: i += 1
-        return False
+        if len(matrix) == 0: return False
+        m, n = len(matrix), len(matrix[0])
+        top, bottom = 0, m - 1
+        while top + 1 < bottom:
+            mid = (top + bottom) / 2
+            if matrix[mid][0] < target: top = mid
+            else: bottom = mid
+        if matrix[top][0] <= target <= matrix[top][n-1]: row = top
+        elif matrix[bottom][0] <= target <= matrix[bottom][n-1]: row = bottom
+        else: return False
+        left, right = 0, n-1
+        while left + 1 < right:
+            mid = (left + right) / 2
+            if matrix[row][mid] < target: left = mid
+            else: right = mid
+        return matrix[row][left] == target or matrix[row][right] == target
