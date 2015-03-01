a6dc4f55470ec84f031d970b4bed05432e9ad1cf
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2015-02-28 23:19:07 -0800

    init commit

--- Compare_Version_Numbers.py
+++ Compare_Version_Numbers.py
@@ -5,20 +5,16 @@ class Solution:
     def compareVersion(self, version1, version2):
         n1 = [int(i) for i in version1.split('.')]
         n2 = [int(i) for i in version2.split('.')]
-        l = min(len(n1), len(n2))
+        return self.compareVersionHelper(n1, n2)
+        
+    def compareVersionHelper(self, n1, n2):
+        if len(n1) > len(n2): return -self.compareVersionHelper(n2, n1)
         i = 0
-        while i < l:
+        while i < len(n1):
             if n1[i] > n2[i]: return 1
             elif n1[i] < n2[i]: return -1
             i += 1
-        if len(n1) > len(n2):
-            while i < len(n1):
-                if n1[i] != 0: return 1
-                i += 1
-            return 0
-        elif len(n1) < len(n2):
-            while i < len(n2):
-                if n2[i] != 0: return -1
-                i += 1
-            return 0
-        else: return 0
+        while i < len(n2):
+            if n2[i] != 0: return -1
+            i += 1
+        return 0
