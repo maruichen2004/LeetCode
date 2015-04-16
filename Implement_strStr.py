5926b24a71ea4af58bcf5d64683e9ee04a04657c
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2015-04-15 21:36:09 -0700

    init commit

--- Implement_strStr.py
+++ Implement_strStr.py
@@ -4,13 +4,13 @@ class Solution:
     # @return a string or None
     def strStr(self, haystack, needle):
         len1, len2 = len(haystack), len(needle)
-        if len2 == 0: return haystack
-        if len1 < len2: return None
+        if len2 == 0: return 0
+        if len1 < len2: return -1
         for i in range(len1 - len2 + 1):
             j, k = i, 0
             while k < len2:
                 if haystack[j] == needle[k]:
                     j, k = j + 1, k + 1
                 else: break
-            if k == len2: return haystack[i:]
-        return None
+            if k == len2: return i
+        return -1
