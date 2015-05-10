a83df18c26099c53eb46a5f3dbd757510536d332
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2015-05-09 21:27:44 -0700

    init commit

--- Palindrome_Partitioning_II.py
+++ Palindrome_Partitioning_II.py
@@ -1,20 +1,12 @@
 class Solution:
-    # @param s, a string
-    # @return an integer
+    # @param {string} s
+    # @return {integer}
     def minCut(self, s):
-        lookup = [[False for j in range(len(s))] for i in range(len(s))]
-        mincut = [0 for i in range(len(s))]
+        isPalindrome = [[False for i in range(len(s))] for j in range(len(s))]
+        T = [len(s) - 1 - i for i in range(len(s) + 1)]
         for i in reversed(range(len(s))):
             for j in range(i, len(s)):
-                if s[i] == s[j]  and (j - i < 2 or lookup[i + 1][j - 1]):
-                    lookup[i][j] = True
-        for i in range(len(s)):
-            min_so_far = 2**32 - 1
-            if lookup[0][i] == False:
-                for j in range(i):
-                    if lookup[j + 1][i] == True:
-                        min_so_far = min(min_so_far, mincut[j] + 1)
-            else:
-                min_so_far = 0
-            mincut[i] = min_so_far
-        return mincut[-1]
+                if s[i] == s[j] and (j - i < 2 or isPalindrome[i+1][j-1]):
+                    isPalindrome[i][j] = True
+                    T[i] = min(T[i], 1 + T[j+1])
+        return T[0]
