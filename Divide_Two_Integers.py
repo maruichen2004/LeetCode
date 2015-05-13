deacd062de2b68b08352e9eb0a0922ebc01af569
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2015-05-12 20:48:37 -0700

    init commit

--- Divide_Two_Integers.py
+++ Divide_Two_Integers.py
@@ -1,6 +1,7 @@
 class Solution:
     # @return an integer
     def divide(self, dividend, divisor):
+        inf = 2 ** 31 - 1
         sign = 1 if (dividend > 0 and divisor > 0) or (dividend < 0 and divisor < 0) else -1
         dividend, divisor = abs(dividend), abs(divisor)
         q = 0
@@ -9,6 +10,7 @@ class Solution:
             k = 0
             while dividend >= tmp:
                 q += 1 << k
+                if q > inf: return inf if sign == 1 else -inf - 1
                 dividend -= tmp
                 tmp <<= 1
                 k += 1
