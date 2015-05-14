2a9baedf5f7c9278b8a7b9420aad791d973b2130
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2015-05-13 22:05:11 -0700

    init commit

---
+++ Read_N_Characters_Given_Read4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def read(self, buf, n):
+        if n == 0: return 0
+        sofar = 0
+        buf4 = ["", "", "", ""]
+        nRead = self.read4(buf4)
+        while n Read != 0 and sofar < n:
+            if nRead + sofar >= n:
+                for i in range(n - sofar):
+                    buf[sofar + i] = buf4[i]
+                return n
+            else:
+                for i in range(nRead):
+                    buf[sofar + i] = buf4[i]
+                sofar += nRead
+                buf4 = ["", "", "", ""]
+                nRead = self.read4(buf4)
+        return sofar
