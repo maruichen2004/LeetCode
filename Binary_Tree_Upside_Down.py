380a0407a539b294826dc5d78f16d0f3992cace8
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2015-05-15 22:50:21 -0700

    init commit

--- Binary_Tree_Upside_Down.py
+++ Binary_Tree_Upside_Down.py
@@ -6,18 +6,11 @@ class TreeNode:
         
 class Solution:
     def upsideDownBinaryTree(self, root):
-        tmp, newRoot = None, [None]
-        tmp = self.upsideDownBinaryTreeHelper(root, newRoot)
-        return newRoot[0]
-    
-    def upsideDownBinaryTreeHelper(self, root, newRoot):
-        if root is None: return None
-        if root.left is None and root.right is None:
-            newRoot[0] = root
-            return root
-        parent = self.upsideDownBinaryTreeHelper(root.left, newRoot)
-        parent.left = root.right
-        parent.right = root
-        root.left = None
-        root.right = None
-        return parent.right
+        p, parent, parent_right = root, None, None
+        while p:
+            left = p.left
+            p.left = parent_right
+            parent_right = p.right
+            p.right = parent
+            p = left
+        return parent
