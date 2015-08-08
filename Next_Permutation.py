852de0117ad4d2e2db5a6b00783d68403cec9d1f
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2015-08-07 20:55:59 -0700

    init commit

--- Next_Permutation.py
+++ Next_Permutation.py
@@ -5,8 +5,10 @@ class Solution:
         k, l = -1, 0
         for i in range(len(num) - 1):
             if num[i] < num[i + 1]: k = i
-        if k == -1: return num[::-1]
+        if k == -1:
+            num.reverse()
+            return
         for i in range(len(num)):
             if num[i] > num[k]: l = i
         num[l], num[k] = num[k], num[l]
-        return num[:k+1] + num[:k:-1]
+        num[k+1:] = num[:k:-1]
