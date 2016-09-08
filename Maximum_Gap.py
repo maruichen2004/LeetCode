c875573163270b1ed2b6d56dbd7352f87cb163c7
maruichen2004 <maruichen2011@gmail.com>
2016-09-07 22:30:08 -0700

    init commit

--- Maximum_Gap.py
+++ Maximum_Gap.py
@@ -1,32 +1,18 @@
-class Solution:
-    # @param num, a list of integer
-    # @return an integer
-    def countsort(self, num, nd):
-        count = [0] * 10
-        n = len(num)
-        for i in range(0, n):
-            count[(num[i]/nd) %10] += 1
-        for i in range(1,10):
-            count[i] += count[i-1]
-        output = [0] * n
-        for i in range(n-1, -1, -1):
-            output[count[(num[i]/nd)%10 ] - 1] = num[i]
-            count[(num[i]/nd) % 10] -= 1
-        return output
-     
-    def radixsort(self, num):
-        max_n = max(num)
-        nd = 1
-        while max_n / nd > 0:
-            num = self.countsort(num, nd)
-            nd = nd * 10
-        return num
-         
-    def maximumGap(self, num):
-        if len(num) < 2:
+class Solution(object):
+    def maximumGap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2:
             return 0
-        num = self.radixsort(num)
-        res = abs(num[1] - num[0])
-        for i in xrange(2,len(num)):
-            res = max(res, abs(num[i] - num[i-1]))
-        return res
+        max_n, min_n = max(nums), min(nums)
+        max_g = (max_n - min_n) / (len(nums)-1)
+        b_size = max_g + 1
+        buckets = [[] for i in range((max_n - min_n)/b_size + 1)]
+        for num in nums:
+            buckets[(num-min_n)/b_size].append(num)
+        buckets = [b for b in buckets if b]
+        for i in range(1, len(buckets)):
+            max_g = max(max_g, min(buckets[i]) - max(buckets[i-1]))
+        return max_g
