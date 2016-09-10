d0e6afb3f9de3c1e5ce2f5e9ba293b30f48743d6
maruichen2004 <maruichen2011@gmail.com>
2016-09-10 11:33:47 -0700

    init commit

--- The_Skyline_Problem.py
+++ The_Skyline_Problem.py
@@ -14,20 +14,20 @@ class Solution(object):
         #if either some new building is not processed or live building queue is not empty
         while i < n or heap:
             #the next new building starts before the top one ends, process the new building in the vector
-            if len(heap) == 0 or i < n and buildings[i][0] <= -heap[0][1]:
+            if len(heap) == 0 or i < n and buildings[i][0] <= heap[0][1]:
                 #next timing point to process
                 x = buildings[i][0]
                 #go through all the new buildings that starts at the same point
                 while i < n and buildings[i][0] == x:
                     #just push them in the queue
-                    heapq.heappush(heap, (-buildings[i][2], -buildings[i][1]))
+                    heapq.heappush(heap, (-buildings[i][2], buildings[i][1]))
                     i += 1
             #the current tallest building will end before the next timing point
             else:
                 #next timing point to process
-                x = -heap[0][1]
+                x = heap[0][1]
                 #pop up the processed buildings, i.e. those  have height no larger than cur_H and end before the top one
-                while heap and -heap[0][1] <= x:
+                while heap and heap[0][1] <= x:
                     heapq.heappop(heap)
             #outut the top one
             height = len(heap) and -heap[0][0]
