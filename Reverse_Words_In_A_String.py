821c0f87ad0eea17e0d39f024a3d583c3b78b676
maruichen2004 <maruichen2011@gmail.com>
2016-09-10 13:45:58 -0700

    init commit

--- Reverse_Words_In_A_String.py
+++ Reverse_Words_In_A_String.py
@@ -1,16 +1,31 @@
-class Solution:
-    # @param s, a string
-    # @return a string
+class Solution(object):
     def reverseWords(self, s):
-        res, word = "", ""
-        for ch in s:
-            if ch != " ": word += ch
-            else:
-                if len(word) != 0:
-                    if res != "": res = " " + res
-                    res = word + res
-                    word = ""
-        if len(word) != 0:
-            if res != "": res = " " + res
-            res = word + res
-        return res
+        """
+        :type s: str
+        :rtype: str
+        """
+        strs = [c for c in s]
+        i, j, cnt = 0, 0, 0
+        while 1:
+            while i < len(strs) and strs[i] == ' ':
+                i += 1
+            if i == len(strs):
+                break
+            if cnt:
+                strs[j] = ' '
+                j += 1
+            k = j
+            while i < len(strs) and strs[i] != ' ':
+                strs[j] = strs[i]
+                i, j = i + 1, j + 1
+            self.reverse(strs, k, j - 1)
+            cnt += 1
+        strs = strs[:j]
+        self.reverse(strs, 0, j-1)
+        return ''.join(strs)
+        
+    def reverse(self, s, l, r):
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
