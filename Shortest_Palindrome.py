2499fe3c79354ecdf0ee3022bad57b78c618712a
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2016-09-12 22:32:49 -0700

    init commit

--- Shortest_Palindrome.py
+++ Shortest_Palindrome.py
@@ -2,18 +2,7 @@ class Solution:
     # @param {string} s
     # @return {string}
     def shortestPalindrome(self, s):
-        if not s: return s
-        A = s + s[::-1]
-        prefix = self.getPrefix(A)
-        return s[prefix[-1]+1:][::-1] + s
-        
-    def getPrefix(self, s):
-        prefix = [-1] * len(s)
-        j = -1
-        for i in range(1, len(s)):
-            while j > -1 and s[j+1] != s[i]:
-                j = prefix[j]
-            if s[j+1] == s[i]:
-                j += 1
-            prefix[i] = j
-        return prefix
+        r = s[::-1]
+        for i in range(len(s) + 1):
+            if s.startswith(r[i:]):
+                return r[:i] + s
