06cc66213ccb281fc16a12ee51a39bdd6534d256
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2016-09-12 23:13:34 -0700

    init commit

--- Bitwise_AND_of_Numbers_Range.py
+++ Bitwise_AND_of_Numbers_Range.py
@@ -3,14 +3,9 @@ class Solution:
     # @param {integer} n
     # @return {integer}
     def rangeBitwiseAnd(self, m, n):
-        if m == n or m == 0:
-            return m
-        temp = 1
-        while temp < m:
-            temp <<= 1
-            if m < temp and temp <= n:
-                return 0
-        ans = m
-        for i in xrange(m + 1, n + 1):
-            ans = ans & i
-        return ans
+        i = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            i += 1
+        return m << i
