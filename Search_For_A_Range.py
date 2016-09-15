18967671f134b8ac16a0c41eafabb5a451376429
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2016-09-14 23:51:43 -0700

    init commit

--- Search_For_A_Range.py
+++ Search_For_A_Range.py
@@ -1,21 +1,27 @@
-class Solution:
-    # @param A, a list of integers
-    # @param target, an integer to be searched
-    # @return a list of length 2, [index1, index2]
-    def searchRange(self, A, target):
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         res = [-1, -1]
-        low, high = 0, len(A) - 1
-        while low < high:
-            mid = (low + high) / 2
-            if A[mid] == target: high = mid
-            elif A[mid] > target: high = mid - 1
-            else: low = mid + 1
-        if A[low] != target: return res
-        res[0] = low
-        high = len(A) - 1
-        while low < high:
-            mid = (low + 1 + high) / 2
-            if A[mid] == target: low = mid
-            else: high = mid - 1
-        res[1] = low
+        l, r = 0, len(nums)-1
+        while l < r:
+            mid = (l + r) / 2
+            if nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid
+        if nums[l] != target:
+            return res
+        res[0] = l
+        r = len(nums)
+        while l < r:
+            mid = (l + r) / 2
+            if nums[mid] > target:
+                r = mid
+            else:
+                l = mid + 1
+        res[1] = l-1
         return res
