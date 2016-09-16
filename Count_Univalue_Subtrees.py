7c17e160fdc901a2a500185480fe95ceb6dde434
maruichen2004 <maruichen2011@gmail.com>
2016-09-16 12:44:53 -0700

    init commit

--- Count_Univalue_Subtrees.py
+++ Count_Univalue_Subtrees.py
@@ -11,12 +11,12 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        return self.helper(root, True)[1]
+        return self.dfs(root)[1]
         
-    def helper(self, root, isUni):
+    def dfs(self, root):
         if root is None:
             return True, 0
-        left, left_val = self.helper(root.left, True)
-        right, right_val = self.helper(root.right, True)
-        uni = left and right and (root.val == root.left.val if root.left else True) and (root.val == root.right.val if root.right else True)
+        left, left_val = self.dfs(root.left)
+        right, right_val = self.dfs(root.right)
+        uni = left and right and (root.left.val == root.val if root.left else True) and (root.right.val == root.val if root.right else True)
         return uni, left_val + right_val + uni
