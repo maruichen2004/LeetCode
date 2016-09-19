18c4da0c82daffd7c9cba3c67052d151dc5d575f
maruichen2004 <maruichen2011@gmail.com>
2016-09-19 00:00:01 -0700

    init commit

--- Convert_Sorted_List_To_Binary_Search_Tree.py
+++ Convert_Sorted_List_To_Binary_Search_Tree.py
@@ -1,32 +1,35 @@
-# Definition for a  binary tree node
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-#
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    # @param head, a list node
-    # @return a tree node
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
     def sortedListToBST(self, head):
-        cur, length = head, 0
-        while cur:
-            cur, length = cur.next, length + 1
-        self.head = head
-        return self.sortedListToBSTRec(0, length - 1)
-        
-    def sortedListToBSTRec(self, l, r):
-        if l > r: return None
-        mid = (l + r) / 2
-        left = self.sortedListToBSTRec(l, mid - 1)
-        root = TreeNode(self.head.val)
-        root.left = left
-        self.head = self.head.next
-        root.right = self.sortedListToBSTRec(mid + 1, r)
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+        if head is None:
+            return None
+        if head.next is None:
+            return TreeNode(head.val)
+        slow, fast, last = head, head, head
+        while fast.next and fast.next.next:
+            last = slow
+            slow = slow.next
+            fast = fast.next.next
+        fast = slow.next
+        last.next = None
+        root = TreeNode(slow.val)
+        if head != slow:
+            root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(fast)
         return root
