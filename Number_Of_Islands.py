051b8e389a3227216175547d2f5896021be7cfe0
maruichen2004 <maruichen2011@gmail.com>
2016-09-21 13:09:46 -0700

    init commit

--- Number_Of_Islands.py
+++ Number_Of_Islands.py
@@ -1,21 +1,25 @@
-class Solution:
-    # @param {character[][]} grid
-    # @return {integer}
+class Solution(object):
     def numIslands(self, grid):
-        if len(grid) == 0: return 0
-        visited = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if len(grid) == 0:
+            return 0
+        visited = [[False for j in range(len(grid[0]))] for i in range(len(grid))]
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and visited[i][j] == False:
+                if grid[i][j] == '1' and not visited[i][j]:
                     self.dfs(grid, visited, i, j)
                     count += 1
         return count
         
     def dfs(self, grid, visited, i, j):
-        if grid[i][j] == "0" or visited[i][j] == True: return
+        if visited[i][j]:
+            return
         visited[i][j] = True
-        if i != 0: self.dfs(grid, visited, i-1, j)
-        if i != len(grid) - 1: self.dfs(grid, visited, i+1, j)
-        if j != 0: self.dfs(grid, visited, i, j-1)
-        if j != len(grid[0]) - 1: self.dfs(grid, visited, i, j+1)
+        for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+            if not 0 <= x < len(grid) or not 0 <= y < len(grid[0]) or grid[i][j] != '1':
+                continue
+            self.dfs(grid, visited, x, y)
--- Unique_Binary_Search_Trees_II.py
+++ Unique_Binary_Search_Trees_II.py
@@ -1,26 +1,32 @@
-# Definition for a  binary tree node
-# class TreeNode:
+# Definition for a binary tree node.
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    # @return a list of tree node
+class Solution(object):
     def generateTrees(self, n):
-        return self.generateTreesRec(1, n)
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        if n == 0:
+            return []
+        return self.dfs(1, n)
         
-    def generateTreesRec(self, low, high):
+    def dfs(self, start, end):
         res = []
-        if low > high: res.append(None)
+        if start > end:
+            res.append(None)
         else:
-            for i in range(low, high + 1):
-                left = self.generateTreesRec(low, i - 1)
-                right = self.generateTreesRec(i + 1, high)
-                for j in left:
-                    for k in right:
+            for i in range(start, end+1):
+                left = self.dfs(start, i-1)
+                right = self.dfs(i+1, end)
+                for l in left:
+                    for r in right:
                         root = TreeNode(i)
-                        root.left = j
-                        root.right = k
+                        root.left = l
+                        root.right = r
                         res.append(root)
         return res
