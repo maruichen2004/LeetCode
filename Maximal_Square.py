3584a1316d909d54eeba9a34f15f1ee0f6af3827
maruichen2004 <maruichen2011@gmail.com>
2016-09-23 22:03:27 -0700

    init commit

--- Maximal_Square.py
+++ Maximal_Square.py
@@ -1,21 +1,24 @@
-class Solution:
-    # @param {character[][]} matrix
-    # @return {integer}
+class Solution(object):
     def maximalSquare(self, matrix):
-        if not matrix: return 0
-        dp = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
-        max_size = 0
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return 0
+        dp = [[0 for j in range(len(matrix[0]))] for j in range(len(matrix))]
+        length = 0
+        for i in range(len(matrix)):
+            if matrix[i][0] == '1':
+                dp[i][0] = 1
+                length = max(length, dp[i][0])
         for j in range(len(matrix[0])):
             if matrix[0][j] == '1':
                 dp[0][j] = 1
-                max_size = max(max_size, dp[0][j])
+                length = max(length, dp[0][j])
         for i in range(1, len(matrix)):
-            if matrix[i][0] == '1':
-                dp[i][0] = 1
             for j in range(1, len(matrix[0])):
                 if matrix[i][j] == '1':
                     dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-                    max_size = max(max_size, dp[i][j])
-                else:
-                    dp[i][j] = 0
-        return max_size * max_size
+                    length= max(length, dp[i][j])
+        return length * length
