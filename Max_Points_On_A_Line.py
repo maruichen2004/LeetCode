64497b73645527c7974d031d33686eb78488ba58
maruichen2004 <maruichen2011@gmail.com>
2016-09-30 23:28:01 -0700

    init commit

--- Max_Points_On_A_Line.py
+++ Max_Points_On_A_Line.py
@@ -10,11 +10,14 @@ class Solution(object):
         :type points: List[Point]
         :rtype: int
         """
-        d = collections.defaultdict(list)
+        if len(points) <= 1:
+            return len(points)
+        d = collections.defaultdict(set)
         for p, q in itertools.combinations(points, 2):
             if p.x == q.x:
                 t = (float('inf'), p.x)
             else:
-                t = ((p.y-q.y)/float((p.x-q.x)), (p.x*q.y-q.x*p.y)/float(p.x-q.x))
-            d[t] += [p, q]
-        return max(len(set(l)) for l in d.values()) if len(points) > 1 else len(points)
+                t = ((p.y-q.y)/float((p.x-q.x)), (p.x*q.y - q.x*p.y)/float((p.x-q.x)))
+            d[t].add(p)
+            d[t].add(q)
+        return max(len(l) for l in d.values())
--- Maximum_Gap.py
+++ Maximum_Gap.py
@@ -4,15 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) < 2:
+        if len(nums) == 0:
             return 0
-        max_n, min_n = max(nums), min(nums)
-        max_g = (max_n - min_n) / (len(nums)-1)
-        b_size = max_g + 1
-        buckets = [[] for i in range((max_n - min_n)/b_size + 1)]
+        n = len(nums)
+        mx, mn = float('-inf'), float('inf')
         for num in nums:
-            buckets[(num-min_n)/b_size].append(num)
-        buckets = [b for b in buckets if b]
-        for i in range(1, len(buckets)):
-            max_g = max(max_g, min(buckets[i]) - max(buckets[i-1]))
-        return max_g
+            mx = max(num, mx)
+            mn = min(num, mn)
+        bucket_size = (mx - mn) / n + 1
+        bucket_num = (mx - mn) / bucket_size + 1
+        bucket_min = [float('inf')] * bucket_num
+        bucket_max = [float('-inf')] * bucket_num
+        bucket_used = set()
+        for num in nums:
+            idx = (num - mn) / bucket_size
+            bucket_min[idx] = min(bucket_min[idx], num)
+            bucket_max[idx] = max(bucket_max[idx], num)
+            bucket_used.add(idx)
+        i, res = 0, 0
+        for j in range(1, bucket_num):
+            if j not in bucket_used:
+                continue
+            res = max(res, bucket_min[j] - bucket_max[i])
+            i = j
+        return res
