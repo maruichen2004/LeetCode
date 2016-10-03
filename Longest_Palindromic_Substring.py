ff47738ed9c4d5c9d8d891dfa142cf15b75ff56c
maruichen2004 <maruichen2011@gmail.com>
2016-10-02 20:50:51 -0700

    init commit

--- Longest_Palindromic_Substring.py
+++ Longest_Palindromic_Substring.py
@@ -4,21 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        if len(s) == 0:
+            return ""
         n = len(s)
-        longest, mid = "", (n - 1 )/2
+        res, mid = "", (n-1)/2
         i, j = mid, mid
         while i >= 0 and j < n:
             args = [(i, i), (i, i+1), (j, j), (j, j+1)]
             for l, r in args:
                 tmp = self.helper(s, l, r)
-                if len(tmp) > len(longest):
-                    longest = tmp
-            if len(longest) >= 2 * i:
-                return longest
+                if len(tmp) > len(res):
+                    res = tmp
             i, j = i - 1, j + 1
-        return longest
+        return res
         
     def helper(self, s, l, r):
         while l >= 0 and r < len(s) and s[l] == s[r]:
-            l, r = l - 1, r + 1
+            l -= 1
+            r += 1
         return s[l+1:r]
